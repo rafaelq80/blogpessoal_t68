e4a8a5aff8eadade1e02846d5d388b8da7dfe746
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-12-08 11:37:27 -0300

    Projeto Blog Pessoal - Listar todas as Postagens

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,69 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens") // CREATE TABLE tb_postagens
+public class Postagem {
+
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O Atributo título é obrigatório!")
+	@Size(min = 5, max = 100, message="O Atributo título deve conter no mínimo 5 e no máximo 100 caracteres.")
+	private String titulo;
+	
+	@Column(length = 1000)
+	@NotBlank(message = "O Atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message="O Atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres.")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+}
