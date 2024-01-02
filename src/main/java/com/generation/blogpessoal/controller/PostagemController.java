cce978ae039d15a785a60c89e6321b32b7b15934
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-01-02 15:13:15 -0300

    Testes com JUnit

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -30,67 +31,76 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
+	@Autowired
+	private TemaRepository temaRepository;
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
+	public ResponseEntity<List<Postagem>> getAll() {
 		return ResponseEntity.ok(postagemRepository.findAll());
-		
+
 		// SELECT * FROM tb_postagens;
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Postagem> getById(@PathVariable Long id){
-		
-		/*Optional<Postagem> resposta = postagemRepository.findById(id);
-		
-		if (resposta.isPresent())
-			return ResponseEntity.ok(resposta);
-		else
-			return ResponseEntity.notFound().build();*/
-		
-		return postagemRepository.findById(id)
-				.map(resposta -> ResponseEntity.ok(resposta))
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+
+		/*
+		 * Optional<Postagem> resposta = postagemRepository.findById(id);
+		 * 
+		 * if (resposta.isPresent()) return ResponseEntity.ok(resposta); else return
+		 * ResponseEntity.notFound().build();
+		 */
+
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		
+
 		// SELECT * FROM tb_postagens WHERE id = ?;
-		
+
 	}
-	
+
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(postagemRepository.save(postagem));
-		
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+
+		if (temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
 		// INSERT INTO tb_postagens (titulo, texto) VALUES (?, ?);
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
-		
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.ok(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-		
-		// UPDATE tb_postagens SET titulo = ?,  texto = ? WHERE id = ?;
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+
+		if (postagemRepository.existsById(postagem.getId())) {
+
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+		}
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		// UPDATE tb_postagens SET titulo = ?, texto = ? WHERE id = ?;
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
-		
+
 		Optional<Postagem> postagem = postagemRepository.findById(id);
 
 		if (postagem.isEmpty())
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
 		postagemRepository.deleteById(id);
-		
+
 		// DELETE FROM tb_postagens WHERE id = ?;
 	}
-	
+
 }
---
+++ src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
@@ -0,0 +1,128 @@
+package com.generation.blogpessoal.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.service.UsuarioService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioControllerTest {
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@BeforeAll
+	void start() {
+		
+		usuarioRepository.deleteAll();
+		
+		usuarioService.cadastrarUsuario(new Usuario(0L,
+				"Root", "root@root.com", "rootroot", " "));
+	}
+	
+	@Test
+	@DisplayName("😀 Cadastrar Usuário")
+	public void deveCriarUmUsuario() {
+		
+		/* Corpo da Requisição */
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario( 0L,
+				"Gabriel Rodrigues", "gabriel@email.com.br", "12345678", ""));
+		
+		/* Requisição HTTP */
+		
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate
+				.exchange("/usuarios/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+		
+		/* Verifica o HTTP Status Code */
+		assertEquals(HttpStatus.CREATED, corpoResposta.getStatusCode());
+		
+	}
+	
+	@Test
+	@DisplayName("😀 Não Deve Duplicar Usuário")
+	public void naoDeveDuplicarUsuario() {
+		
+		usuarioService.cadastrarUsuario(new Usuario( 0L,
+				"Amanda Tsai", "amanda@email.com.br", "12345678", ""));
+		
+		/* Corpo da Requisição */
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario( 0L,
+				"Amanda Tsai", "amanda@email.com.br", "12345678", ""));
+		
+		/* Requisição HTTP */
+		
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate
+				.exchange("/usuarios/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+		
+		/* Verifica o HTTP Status Code */
+		assertEquals(HttpStatus.BAD_REQUEST, corpoResposta.getStatusCode());
+		
+	}
+	
+	@Test
+	@DisplayName("😀 Deve Atualizar Usuário")
+	public void deveAtualizarUsuario() {
+		
+		Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(new Usuario( 0L,
+				"Kendal Katherine", "kendal@email.com.br", "12345678", ""));
+		
+		/* Corpo da Requisição */
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario( 
+				usuarioCadastrado.get().getId(), "Kendal Katherine Correia", 
+				"kendalk@email.com.br", "78945612", ""));
+		
+		/* Requisição HTTP */
+		
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate
+				.withBasicAuth("root@root.com", "rootroot")
+				.exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
+		
+		/* Verifica o HTTP Status Code */
+		assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
+		
+	}
+	
+	@Test
+	@DisplayName("😀 Deve Listar todos os Usuários")
+	public void deveListarTodosUsuarios() {
+		
+		usuarioService.cadastrarUsuario(new Usuario( 0L,
+				"Vitor Nascimento", "vitor@email.com.br", "12345678", ""));
+		
+		usuarioService.cadastrarUsuario(new Usuario( 0L,
+				"Samara Almeida", "samara@email.com.br", "12345678", ""));
+		
+		/* Requisição HTTP */
+		
+		ResponseEntity<String> corpoResposta = testRestTemplate
+				.withBasicAuth("root@root.com", "rootroot")
+				.exchange("/usuarios/all", HttpMethod.GET, null, String.class);
+		
+		/* Verifica o HTTP Status Code */
+		assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
+		
+	}
+}
