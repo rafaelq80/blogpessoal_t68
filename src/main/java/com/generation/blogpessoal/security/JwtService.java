e24a0305452f0ae7d5f63eecd5bc6098a27a8c9e
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-01-21 23:43:08 -0300

    Update JwtService.java

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -58,7 +58,7 @@ public class JwtService {
 					.setClaims(claims)
 					.setSubject(userName)
 					.setIssuedAt(new Date(System.currentTimeMillis()))
-					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 240))
 					.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
 
